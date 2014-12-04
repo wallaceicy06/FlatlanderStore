64fd4489b452395acab400cf165234e364eef915
Sean Harger <seanharger93@gmail.com>
2014-12-04 16:14:29 -0600

    Added some loops and more products. Also correctly added bootstrap css.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,23 @@
-var app = angular.module('store', []);
+(function(){
+  var gems = [
+    {
+      name: 'Azurite', 
+      price: 2.95,
+      canPurchase: true,
+      soldOut: false 
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      canPurchase: true,
+      soldOut: false
+    }
+  ];
+
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function() {
+    this.products = gems;
+  });
+})();
+
